7d931f6f53c7787450cfd8c85ecdbaa30e16e8f4
Jamie <jasm1@st-andrews.ac.uk>
2025-11-21 18:09:33 +0000

    Completed Req.p1.3.

---
+++ part-1/HttpRequestHandler.java
@@ -0,0 +1,90 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class HttpRequestHandler {
+    private BufferedReader reader;
+    private OutputStream outputStream;
+
+    private String documentRoot;
+
+    public HttpRequestHandler(BufferedReader reader, OutputStream outputStream, String documentRoot) {
+        this.reader = reader;
+        this.outputStream = outputStream;
+        this.documentRoot = documentRoot;
+    }
+
+    protected void handleRequest() {
+        try {
+            // Read the request
+            String request = reader.readLine();
+            if (request == null || request.isEmpty()) { return; }
+
+            // for debugging
+            System.out.println(request);
+
+            // get the file path
+            String[] parts = request.split(" ");
+            String filePath = parts[1];
+
+            if (filePath.equals("/")) {
+                filePath = documentRoot + "/index.html";
+            } // we want to be able to take '/{something}', check that there exist a file with that name, and return it.
+            else {
+                filePath = documentRoot + "/" + filePath;
+                File file = new File(filePath);
+                if (!file.exists()) {
+                    send404(outputStream);
+                    return;
+                }
+            }
+
+            serveFile(Path.of(filePath), outputStream);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void serveFile(Path path, OutputStream outputStream) throws IOException {
+        try {
+            byte[] fileContent = Files.readAllBytes(path);
+            String fileType = getFileType(path.getFileName().toString());
+
+            outputStream.write(("HTTP/1.1 200 OK\r\n" +
+                    "Content-Type: " + fileType + "\r\n" +
+                    "Content-Length: " + fileContent.length + "\r\n" +
+                    "\r\n").getBytes());
+
+            outputStream.write(fileContent);
+            outputStream.flush();
+        }
+        catch (IOException e) {
+            System.out.printf("Could not serve file '%s': %s%n", path.getFileName(), e.getMessage());
+            send404(outputStream);
+        }
+    }
+
+    private String getFileType(String fileName) {
+        if (fileName.endsWith(".html")) {
+            return "text/html";
+        }
+        else if (fileName.endsWith(".css")) {
+            return "text/css";
+        }
+        else if (fileName.endsWith(".js")) {
+            return "application/javascript";
+        } else { // shouldn't get to this point
+            return "text/plain";
+        }
+    }
+
+    private void send404(OutputStream outputStream) throws IOException {
+        String response = "HTTP/1.1 404 Not Found\r\n\r\n404 File Not Found";
+        outputStream.write(response.getBytes());
+        outputStream.flush();
+    }
+}
--- part-1/HttpServer.java
+++ part-1/HttpServer.java
@@ -1,13 +1,11 @@
 import java.io.*;
 import java.net.*;
-import java.util.List;
 import java.util.Properties;
 
 // used https://www.youtube.com/watch?v=5wQWJAvMDGg
 public class HttpServer {
 
     private static int port;
-    private static String documentRoot; // TODO: is this needed?
 
     private static ServerSocket  server;
     private static Properties prop;
@@ -19,70 +17,30 @@ public class HttpServer {
     public static void main(String[] args) {
         // load in our properties
         Properties prop = PropertiesParser.loadProperties();
+        assert prop != null;
 
         // start the server
-        assert prop != null;
         port = Integer.parseInt(prop.getProperty("serverPort"));
         startServer();
 
         while (true) {
-            try {
+            try (
+                    Socket connection = server.accept();
+                    InputStream inputStream = connection.getInputStream();
+                    OutputStream outputStream = connection.getOutputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            ) {
                 // wait for connection
-                Socket connection = server.accept();
                 System.out.println("New connection: " + connection.getInetAddress());
+                HttpRequestHandler requestHandler = new HttpRequestHandler(reader, outputStream, prop.getProperty("documentRoot"));
 
-                // serve the files provided in starter/public
-                handleRequest(connection);
-
-//                /* input and output */
-//                PrintWriter tx = new PrintWriter(connection.getOutputStream(), true);
-//                BufferedReader rx = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//
-//                /* process the HTTP request */
-//
-//                /* first line is the start-line (see RFC7230 sec 3) */
-//                String startLine = rx.readLine();
-//                System.out.println("HTTP start-line: " + startLine);
-//
-//                /* next come the headers */
-//
-//                Pattern p = Pattern.compile(headerRegex);
-//
-//                /* loop over headers and put them into a HashMap */
-//
-//                String input;
-//                HashMap<String, String> headers = new HashMap<>();
-//                while ((input = rx.readLine()) != null && !input.isEmpty()) {
-//                    Matcher m = p.matcher(input);
-//                    boolean found = m.find();
-//                    if (found) {
-//                        headers.put(m.group("name"),m.group("value"));
-//                    }
-//                }
-//
-//                System.out.println("HTTP headers: " + headers);
-//
-//                /* next is the body but we will ignore it for simplicity */
-//
-//                /* return an inappropriate (?) HTTP status */
-//                /* note that HTTP uses CRLF line breaks, encoded as \r\n */
-//
-//                String response = "HTTP/1.1 418 I'm a teapot\r\n\r\n\r\n";
-//
-//                tx.println(response);
-
-                connection.close();
+                requestHandler.handleRequest();
             }
-
-            // keep the server running; use Ctrl-C to quit the while loop
-
             catch (IOException e) {
                 System.err.println("IO Exception: " + e.getMessage());
             }
         }
     }
-
-
 
     private static void startServer() {
         try {
@@ -91,42 +49,5 @@ public class HttpServer {
         } catch (IOException e) {
             System.err.println("IO Exception: " + e.getMessage());
         }
-    }
-
-    private static void handleRequest(Socket socket) {
-        try(InputStream inputStream = socket.getInputStream();
-            OutputStream outputStream = socket.getOutputStream()){
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String line = reader.readLine();
-            List<String>  parts = List.of(line.split(" "));
-
-            String requestMethod = parts.get(0);
-            String path = parts.get(1);
-
-            if ("GET".equalsIgnoreCase(requestMethod) && "/index.html".equals(path)) {
-                HttpResponse response = HttpResponse.from(parts);
-
-                writeResponse(outputStream, response);
-            }
-
-            socket.close();
-        } catch (IOException e) {
-            System.out.println("IO Exception: " + e.getMessage());
-        }
-    }
-
-    private static void writeResponse(OutputStream outputStream, HttpResponse response) throws IOException {
-        String httpHeader = """
-                HTTP/1.1 %d %s
-                Content-Type: %s
-                Content-Length:%d
-                """
-                .formatted(response.code(), response.status(), response.contentType(), response.content().length);
-
-        outputStream.write(httpHeader.getBytes());
-        outputStream.write(response.content());
-        outputStream.flush();
-        outputStream.close();
     }
 }
