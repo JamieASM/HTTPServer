245d1dd5061e027f42ecd1e3c0f9d8cec5d78c8b
Jamie <jasm1@st-andrews.ac.uk>
2025-11-27 16:13:45 +0000

    Store has JavaDocs and has been refactored

--- part-1/utils/store/Store.java
+++ part-1/utils/store/Store.java
@@ -1,19 +1,34 @@
 package utils.store;
 
 import javax.json.Json;
+import javax.json.JsonReader;
 import javax.json.JsonArray;
 import javax.json.JsonObject;
-import javax.json.JsonReader;
+import javax.json.JsonArrayBuilder;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Stores all information about concerts and purchases
+ */
 public class Store {
-    private HashMap<String, Concert> concerts;
+    private HashMap<Integer, Concert> concerts;
     private HashMap<Integer, Purchase> purchases;
 
+    // counter
+    private final AtomicInteger concertID = new AtomicInteger(0);
+
+    /**
+     * Constructor for the Store class.
+     * @param json The path to the JSON file.
+     */
     public Store(String json) {
         try {
             this.concerts = parseJson(Json.createReader(new FileInputStream(json)));
@@ -24,50 +39,79 @@ public class Store {
         }
     }
 
-    private HashMap<String, Concert> parseJson(JsonReader reader) {
+    /**
+     * Parses the JSON file.
+     * @param reader JSON reader object
+     * @return A hash map where the key is the concert ID, and the value is the concert object.
+     */
+    private HashMap<Integer, Concert> parseJson(JsonReader reader) {
+        // set up
         JsonArray jsonArray = reader.readArray();
+        HashMap<Integer, Concert> map = new HashMap<>();
 
-        HashMap<String, Concert> map = new HashMap<>();
-
+        // iterate through the array
         for (int i = 0; i < jsonArray.size(); i++) {
             JsonObject jsonObject = jsonArray.getJsonObject(i);
 
-            map.put(jsonObject.getString("artist").toLowerCase(), new Concert(
+            int id = concertID.getAndIncrement();
+
+            map.put(id, new Concert(
                     jsonObject.getString("artist"),
                     jsonObject.getInt("count"),
                     jsonObject.getString("venue"),
-                    jsonObject.getString("datetime")
+                    jsonObject.getString("datetime"),
+                    id
             ));
         }
 
         return map;
     }
 
-    public Concert getConcert(String artist) {
-        return concerts.get(artist);
-    }
+    /**
+     * Creates a JSON object that contains every concert in the store.
+     * @return The concerts in the store as represented in JSON.
+     */
+    public JsonObject toJson() {
+        JsonArrayBuilder builder = Json.createArrayBuilder();
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[\n");
-
-        for (String artist : concerts.keySet()) {
-            sb.append(concerts.get(artist).toString());
-            sb.append(",");
+        for (Concert concert : concerts.values()) {
+            builder.add(concert.toJson());
         }
 
-        sb.deleteCharAt(sb.length() - 1);
-        sb.append("]");
-
-        return sb.toString();
+        return Json.createObjectBuilder().add("concerts", builder).build();
     }
 
+    /**
+     * Adds a new purchase to the purchase list.
+     * @param purchase The purchase to be added.
+     */
     public void addPurchase(Purchase purchase) {
         purchases.put(purchase.getId(), purchase);
     }
 
+    /**
+     * Retrieves a concert based on its unique ID.
+     * @param id The ID of the concert.
+     * @return The concert object, or null if the concert does not exist.
+     */
+    public Concert getConcert(int id) {
+        return concerts.getOrDefault(id, null);
+    }
+
+    /**
+     * Retrieves a purchase based on its unique ID.
+     * @param id The ID of the purchase.
+     * @return The purchase object, or null if the concert does not exist.
+     */
     public Purchase getPurchase(Integer id) {
-        return purchases.get(id);
+        return purchases.getOrDefault(id, null);
+    }
+
+    /**
+     * Retrieves the list of all purchases.
+     * @return A list of all purchases made.
+     */
+    public List<Purchase> getPurchases() {
+        return new ArrayList<>(purchases.values());
     }
 }
