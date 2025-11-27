ebdcc0b5eebc0f6360ee9c1be7ca5a99b7c54a95
Jamie <jasm1@st-andrews.ac.uk>
2025-11-27 17:18:09 +0000

    HttpStatus has JavaDocs and has been refactored

--- part-1/utils/request/enums/HttpStatus.java
+++ part-1/utils/request/enums/HttpStatus.java
@@ -1,5 +1,8 @@
-package utils.enums;
+package utils.request.enums;
 
+/**
+ * Enum class to represent HTTP status types and their associated messages.
+ */
 public enum HttpStatus {
     OK(200, "OK"),
     SERVER_ERROR(500, "Internal Server Error"),
@@ -7,20 +10,31 @@ public enum HttpStatus {
     BAD_REQUEST(400, "Bad Request"),
     NOT_FOUND(404, "Not Found");
 
-
-
     private final int statusCode;
     private final String statusMessage;
 
+    /**
+     * Constructor for HttpStatus
+     * @param statusCode The status code.
+     * @param statusMessage The associated message.
+     */
     HttpStatus(int statusCode, String statusMessage) {
         this.statusCode = statusCode;
         this.statusMessage = statusMessage;
     }
 
+    /**
+     * Retrieves the status code.
+     * @return The status code.
+     */
     public int getStatusCode() {
         return statusCode;
     }
 
+    /**
+     * Retrieves the status message.
+     * @return The status message.
+     */
     public String getStatusMessage() {
         return statusMessage;
     }
