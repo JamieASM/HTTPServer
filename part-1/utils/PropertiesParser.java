a6cabb611782b5d0425567f87473c50adc9c2366
Jamie <jasm1@st-andrews.ac.uk>
2025-11-27 15:58:47 +0000

    PropertiesParser has JavaDocs and has been refactored

--- part-1/utils/PropertiesParser.java
+++ part-1/utils/PropertiesParser.java
@@ -2,14 +2,23 @@ package utils;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+
 import java.util.Objects;
 import java.util.Properties;
 
+/**
+ * Parses the 'cs2003-C3.properties' file.
+ */
 public class PropertiesParser {
+
+    /**
+     * Reads 'cs2003-C3.properties'
+     * @return A Properties object.
+     */
     public static Properties loadProperties() {
         try {
             // parse the properties file
-            // from https://www.baeldung.com/java-properties
+            // from https://www.baeldung.com/java-properties TODO
             String rootPath = Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResource("")).getPath();
             String root = "HTTPServer";
 
