f60e767834b47d26ff8d5881b1df18948658b9aa
Jamie <jasm1@st-andrews.ac.uk>
2025-11-27 17:19:28 +0000

    ContentType has JavaDocs and has been refactored

--- part-1/utils/request/enums/ContentType.java
+++ part-1/utils/request/enums/ContentType.java
@@ -1,5 +1,8 @@
-package utils.enums;
+package utils.request.enums;
 
+/**
+ * Enum class representing the type of content a HTTP response contains.
+ */
 public enum ContentType {
     html("text/html"),
     css("text/css"),
@@ -9,10 +12,18 @@ public enum ContentType {
 
     private final String contentType;
 
+    /**
+     * Constructor for the ContentType class.
+     * @param contentType The type of content, as represented by a string.
+     */
     ContentType(String contentType) {
         this.contentType = contentType;
     }
 
+    /**
+     * Retrieves the content type.
+     * @return The content type.
+     */
     public String getContentType() {
         return contentType;
     }
